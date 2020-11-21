5d0b94b5fced7c94bb890d3fe18e2011f5b95b86
Anaji Kadam <anajikadam17@gmail.com>
2020-11-21 22:16:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar  1 12:40:42 2020
-
-@author: Anaji
-"""
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/')
+
+@app.route('/',methods=['GET'])
 def home():
-    return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return "YAY!!! its working"
 
 
+@app.route('/add',methods=['GET'])
+def addNum():
+    a = 2
+    b = 3
+    return "The sm of {} and {} is {}".format(a,b,a+b)
+    
 if __name__ == "__main__":
     app.run(debug=True)
